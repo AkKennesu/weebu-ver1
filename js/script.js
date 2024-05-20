197948283313aa6fc695ca075b8bad0460626511
AkKennesu <93695007+AkKennesu@users.noreply.github.com>
2024-05-20 22:36:00 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+      switchMode = body.querySelector(".none")
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+switchMode.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+    }
+});
+
+
+  
